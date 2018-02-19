26bf22f80e241f03768116141ccb8756015acd54
Annabelle Tang <tangannabelle1@gmail.com>
2018-02-19 14:49:04 -0800

    Update analysisTool.py
    Adding doctstrings to each function, and deleting a section of this code (the edition of this project prior to the final edit that was forgotten).

--- analysisTool.py
+++ analysisTool.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
 import psycopg2
 import time
 import sys
 
 
 def connect():
+    """Connecting this tool to the database called 'news'."""
     return psycopg2.connect("dbname=news")
 
 query1 = "select title, views from view_articles limit 3"
@@ -20,6 +22,8 @@ error_rate from error_days where error_rate > 1.0"
 
 
 def popular3Articles(query1):
+    """This function executes query1 and prints out the title
+    and number of views of the result."""
     db = connect()
     c = db.cursor()
     c.execute(query1)
@@ -39,6 +43,8 @@ def popular3Articles(query1):
 
 
 def popularAuthors(query2):
+    """This funciton executes query2 and prints out the name
+    and quantified popularity of the result."""
     db = connect()
     c = db.cursor()
     c.execute(query2)
@@ -54,6 +60,8 @@ def popularAuthors(query2):
 
 
 def errorDays(query3):
+    """This funciton executes query3 and prints out the date that
+    has the highest error_rate."""
     db = connect()
     c = db.cursor()
     c.execute(query3)
@@ -76,65 +84,3 @@ if __name__ == "__main__":
 
         print('On this date more than 1% of requests lead to errors:', file=f)
         errorDays(query3)
-
-
-def connect():
-    return psycopg2.connect('dbname=news')
-
-# 1. What are the most popular three articles of all time?
-# Present this information as a sorted list with the most popular
-# article at the top
-# Listing "author" because it will be helpful for the second question.
-
-
-query1 = "select * from view_articles limit 3"
-
-
-def popular3Articles():
-    db = connect()
-    c = db.cursor()
-    c.execute(query1)
-    c.fetchall()
-    db.close()
-
-
-# 2. Who are the most popular article authors of all time?
-# Sum up all of the articles each author has written, which authors get
-# the most page views?
-# Present this as a sorted list with the most popular author at the top
-
-
-query2 = "select * from author_popularity"
-
-
-def popularAuthors():
-    db = connect()
-    c = db.cursor()
-    c.execute(query2)
-    c.fetchall()
-    db.close()
-
-
-# 3. On which day did more than 1% of requests lead to errors?
-
-
-query3 = "select * from error_rates limit 1"
-
-
-def errorDays():
-    db = connect()
-    c = db.cursor()
-    c.execute(query3)
-    c.fetchall()
-    db.close()
-
-
-if __name__ == "__main__":
-    print('These 3 articles have been accessed the most:')
-    popular3Articles()
-
-    print('From the most popular to the least popular authors:')
-    popularAuthors()
-
-    print('On this date more than 1% of requests lead to errors:')
-    errorDays()
